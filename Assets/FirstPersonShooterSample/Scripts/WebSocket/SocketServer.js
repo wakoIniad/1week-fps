257696f97bc59f33b5405c3514017466a31125f3
wakoIniad <s1f102400606@iniad.org>
2025-03-20 09:56:08 +0900

    save

---
+++ Assets/FirstPersonShooterSample/Scripts/WebSocket/SocketServer.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const http = require('http');
+
+// Socket.ioをインポート
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+
+// 初期化
+const io = socketIo(server);
+
+const PORT = 3000;
+
+/*app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});*/
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+const connections = {};
+let connectionCounter = 0;
+function makeId() {
+    return connectionCounter.toString();
+}
+// クライアントとのコネクションが確立したら'connected'という表示させる
+io.on('connection', (socket) => {
+    connectionCounter++;
+    console.log('connected');
+    const id = makeId();
+    connections[id] = socket;
+    
+    socket.on('message', (mess) => {
+        console.log(mess.toString());
+    });
+    socket.on('close', () => {
+      console.log('ws close');
+    });
+});
